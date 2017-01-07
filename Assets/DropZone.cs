6fec0770d18772ea7d19b01ab69482b7dbb5897f
trevor-umeda <trevor.umeda@gmail.com>
2017-01-06 18:44:31 -0800

    basic drag and drop tutorial finished

---
+++ Assets/DropZone.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public Draggable.Slot typeOfItem = Draggable.Slot.WEAPON;
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		if (eventData.pointerDrag == null)
+			return;
+		
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
+		if (d != null) {
+			d.placeholderParent = this.transform;
+
+		}
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+		if (eventData.pointerDrag == null)
+			return;
+		
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
+		if (d != null && d.placeholderParent == this.transform) {
+			d.placeholderParent = this.transform;
+
+		}
+	}
+
+	public void OnDrop(PointerEventData eventData) {
+		Debug.Log (eventData.pointerDrag.name + " was ropped on " + gameObject.name);
+
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
+		if (d != null) {
+		//	if (typeOfItem == d.typeOfItem) { 
+				d.parentToReturnTo = this.transform;
+			//}
+				
+		}
+	}
+}
